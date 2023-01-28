103e396b8e33c8996be18be2eb3a38aea2a6c9ad
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-01-28 19:49:08 +0700

    new datatotalquantity.json & mod totalquantity.js

---
+++ Tugas Pekanan 3 - Javascript/totalquantity.js
@@ -0,0 +1,11 @@
+module.export = function TotalQuantity(data){
+    for(let i=0; i<data.quantity.length; i++){
+        let ProductCode = data.productCode;
+        let Quantity = data.quantity[i];
+        totalQuantity = sum(Quantity);
+        console.log("Jumlah quantity product dengan code " + ProductCode + " adalah : " + totalQuantity);
+    }
+}
+
+TotalQuantity(response);
+console.log(response.data);
