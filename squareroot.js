a80b72a559f25b26bed74044cc11727b4acd8925
SadiatunNurulHikmah <diah.hikmah@gmail.com>
2023-01-28 14:19:22 +0700

    update squareroot.js (done)

--- squareroot.js
+++ squareroot.js
@@ -1,11 +1,13 @@
 const prompt = require('prompt-sync')();
 
 console.log("Welcome!");
-var inputAngka = prompt("Masukkan Angka Genap : ");
-const hasil = Math.sqrt(inputAngka);
+var counter = 1;
+var ulangi = true;
 
-
-
+while(ulangi){
+    console.log("Perhitungan ke-"+counter)
+    var inputAngka = prompt("Masukkan Angka Genap : ");
+    const hasil = Math.sqrt(inputAngka);
     if (inputAngka % 2 == 0) {
         console.log(`Akar Pangkat Dua dari ${inputAngka} adalah ${hasil}`);
     } else if (inputAngka < 0) {
@@ -13,5 +15,16 @@ const hasil = Math.sqrt(inputAngka);
     } else {
         console.log('Tidak bisa input bilangan ganjil');
     }
+    
+    var jawab = prompt("Apakah anda mau mengulang? (y/n) : ") //Input 1 atau yes atau 0 atau no
+    counter++;
 
+    if(jawab == true || jawab == "y"){
+       ulangi = true;
+    } else {
+        ulangi = false;
+    }
+
+}
+    
 
